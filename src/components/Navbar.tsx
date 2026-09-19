5f1a7421076e5200b72e4479509c45ee6a5cb72f
Jahid Hasib <cse1095.hasib@gmail.com>
2026-09-20 03:07:30 +0600

    Navbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../assets/logo.png';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-green-200">
+                <div className='max-w-7xl container mx-auto flex justify-between items-center p-4'>
+                <img src={Logo} alt="Logo"></img>
+                <ul className='flex gap-4 items-center'>
+                    <li>Home</li>
+                    <li>Fixture</li>
+                    <li>Teams</li>
+                    <li>Schedules</li>
+                </ul>
+        </div>
+            </nav>
+    );
+};
+
+export default Navbar;
