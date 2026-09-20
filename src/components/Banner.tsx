f101fb8f08f916285d0d48e7117e81f6712d0dd7
Jahid Hasib <cse1095.hasib@gmail.com>
2026-09-20 17:46:32 +0600

    Banner section added

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="bg-indigo-400 min-h-[400px] my-6 flex justify-center items-center">
+            <h2 className="font-bold text-6xl">Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
